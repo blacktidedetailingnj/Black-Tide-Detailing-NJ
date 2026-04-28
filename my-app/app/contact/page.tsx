9cd46c86cf2d566cec576845d1f6c565f65ee01e
usabah <uriyasabah309@gmail.com>
2026-04-27 21:40:30 -0400

    added facebook link

--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -46,6 +46,22 @@ const CONTACT_ITEMS = [
       </svg>
     ),
   },
+  {
+    label: "Facebook",
+    value: "BlackTide Detailing",
+    href: "https://www.facebook.com/people/BlackTide-Detailing/pfbid0d6zH66aJWX4nb9dYsyZtUd8h7FYTfvetfwbxvxUhGHSVyajyozG7EDT7fAGYNw6Tl/",
+    icon: (
+      <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
+        <path
+          d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3V2z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    ),
+  },
 ];
 
 export default function ContactPage() {
